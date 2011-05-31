2f78b1e68de497d58c86a4039eab27849aa30c68
wencheng.fang@gmail.com <wencheng.fang@gmail.com@2caf8a10-2d4c-0410-9796-753946c291a4>
2011-05-31 10:45:18 +0000

    export log in release version

--- launcher/bin/zip/chenance.bat
+++ launcher/bin/zip/chenance.bat
@@ -1,3 +1,3 @@
 cd lib
-java -Djava.library.path=. -classpath chenance-gui-${chenance.gui.version}.jar;chenance-data-${chenance.data.version}.jar cn.sh.fang.chenance.MainWindow
+java -Dlog4j.configuration=log4j.release.properties -Djava.library.path=. -classpath chenance-gui-${chenance.gui.version}.jar;chenance-data-${chenance.data.version}.jar cn.sh.fang.chenance.MainWindow
 pause
--- launcher/bin/zip/chenance.sh
+++ launcher/bin/zip/chenance.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-exec java -Djava.library.path=./lib/ -DXstartOnFirstThread \
+exec java \
+	-Dlog4j.configuration=log4j.release.properties \ 
+	-Djava.library.path=./lib/ -DXstartOnFirstThread \
     -classpath chenance-gui-${chenance.gui.version}.jar:chenance-data-${chenance.data.version}.jar \
     cn.sh.fang.chenance.MainWindow
